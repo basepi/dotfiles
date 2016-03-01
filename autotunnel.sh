27a0d7803bf37dcd542edd3a508caf9ddf0565b7
Colton Myers <colton.myers@gmail.com>
2016-03-01 08:43:32 -0700

    Don't output tunnel information by default

--- autotunnel.sh
+++ autotunnel.sh
@@ -18,16 +18,16 @@ TUNNEL2_REMOTEPORT=6667
 
 createTunnel() {
     /usr/bin/ssh -f -N -i $KEYPATH -L$SSH_LOCALPORT:$REMOTEHOST:$SSH_REMOTEPORT -L$TUNNEL1_LOCALPORT:$TUNNEL1_REMOTEHOST:$TUNNEL1_REMOTEPORT -L$TUNNEL2_LOCALPORT:$TUNNEL2_REMOTEHOST:$TUNNEL2_REMOTEPORT $REMOTEUSER@$REMOTEHOST
-    if [[ $? -eq 0 ]]; then
-        echo Tunnel to $REMOTEHOST created successfully
-    else
-        echo An error occurred creating a tunnel to $REMOTEHOST RC was $?
-    fi
+#    if [[ $? -eq 0 ]]; then
+#        echo Tunnel to $REMOTEHOST created successfully
+#    else
+#        echo An error occurred creating a tunnel to $REMOTEHOST RC was $?
+#    fi
 }
 
 ## Run the 'ls' command remotely.  If it returns non-zero, then create a new connection
 /usr/bin/ssh -p $SSH_LOCALPORT $REMOTEUSER@localhost ls >/dev/null 2>&1
 if [[ $? -ne 0 ]]; then
-    echo Creating new tunnel connection
+#    echo Creating new tunnel connection
     createTunnel
 fi
