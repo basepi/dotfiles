3effe55fa877833c94051fd2a1bf5a756c6b74d5
Colton Myers <colton.myers@gmail.com>
2020-01-29 19:15:26 -0700

    Add tab completion for serverless

---
+++ misc/config/tabtab/__tabtab.zsh
@@ -0,0 +1,12 @@
+
+# tabtab source for serverless package
+# uninstall by removing these lines
+[[ -f ~/.config/tabtab/serverless.zsh ]] && . ~/.config/tabtab/serverless.zsh || true
+
+# tabtab source for sls package
+# uninstall by removing these lines
+[[ -f ~/.config/tabtab/sls.zsh ]] && . ~/.config/tabtab/sls.zsh || true
+
+# tabtab source for slss package
+# uninstall by removing these lines
+[[ -f ~/.config/tabtab/slss.zsh ]] && . ~/.config/tabtab/slss.zsh || true
